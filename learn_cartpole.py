8d3135ade89a728b90c2ae2eccabfcd7f2f2119f
romansmit <36138017+romansmit@users.noreply.github.com>
2020-08-18 00:57:43 +0200

    added std exploration and greedy evaluation

--- learn_cartpole.py
+++ learn_cartpole.py
@@ -6,17 +6,21 @@ from tqdm import tqdm
 from tools.qnetwork import QNet
 from tools.multiqnetwork import MultiQNet
 from random import random, randrange
+from math import sin
 
 
 class Agent:
     def __init__(self, mem_size, state_dim, action_dim,
-                 hidden_layers, training_interval, expl_time, eps_decay_time, eps_base, polyak):
+                 hidden_layers, training_interval, expl_time, eps_decay_time, eps_base, polyak,
+                 n_q_copies):
 
         self.memory = StepMemory(mem_size=mem_size, state_dim=state_dim, action_dim=action_dim,
                                  res_sampling=True, discrete_action=True)
         self.training_interval = training_interval
         self.steps_learned = 0
         self.episodes_learned = 0
+
+        self.n_q_copies = n_q_copies
 
         self.batches_trained = 0
 
@@ -26,18 +30,20 @@ class Agent:
         self.eps_base = eps_base
         self.eps_reduce = (1 - eps_base) / eps_decay_time
 
-        self.qnet = MultiQNet(n_copies=3, action_dim=ACTION_DIM, state_dim=STATE_DIM, hidden_layers=hidden_layers,
-                              discount=.97, lr=.01, wgt_decay=.001, polyak=polyak, cuda=False)
+        self.qnet = MultiQNet(n_copies=n_q_copies, action_dim=ACTION_DIM, state_dim=STATE_DIM,
+                              hidden_layers=hidden_layers, discount=.97, lr=.01, wgt_decay=.001, polyak=polyak,
+                              cuda=False)
 
         self.action_dim = action_dim
 
     def __call__(self, obs, greedy):
-        if (not greedy) and random() < self.eps:
-            return randrange(self.action_dim)
-        else:
-            q = self.qnet(obs).mean(axis=0)
-            act = np.argmax(q).item()
-            return act
+        q_copies = self.qnet(obs)
+        q = q_copies.max(axis=0)
+        if not greedy:
+            greed = .5 + .5 * sin(self.episodes_learned)
+            q = greed * q + (1 - greed) * q_copies.std(axis=0)
+        act = np.argmax(q).item()
+        return act
 
     def adjust_eps(self):
         if self.episodes_learned <= self.expl_time:
@@ -69,6 +75,7 @@ class Agent:
 def train_agent(agent, n_episodes, verbose=False, tb_writer=None):
     env = gym.make('CartPole-v0')
     scores = []
+    greedy_scores = []
     scores_temp = []
     action_ratios = []
     for i_ep in tqdm(range(n_episodes)):
@@ -85,9 +92,25 @@ def train_agent(agent, n_episodes, verbose=False, tb_writer=None):
             tb_writer.add_scalar(f'Game/avg_score', avg_score, i_ep)
             tb_writer.add_scalar(f'Game/act0', action_ratio, i_ep)
             tb_writer.add_scalar(f'Game/act1', 1-action_ratio, i_ep)
+        if i_ep + 1 % 1000 == 0:
+            greedy_sc = 0.
+            greedy_action_ratio = 0
+            n_eval = 10
+            for i in range(n_eval):
+                score, actions = play_episode(env, agent, render=False, training=False, verbose=False)
+                greedy_sc += score
+                greedy_action_ratio += actions.mean()
+            greedy_sc /= n_eval
+            greedy_action_ratio /= n_eval
+            greedy_scores.append(greedy_sc)
+            if tb_writer:
+                tb_writer.add_scalar(f'Game/greedy_score', greedy_sc, i_ep)
+                tb_writer.add_scalar(f'Game/greedy_act0', greedy_action_ratio, i_ep)
+                tb_writer.add_scalar(f'Game/greedy_act1', 1-greedy_action_ratio, i_ep)
+
     env.close()
     if verbose:
-        print('Scores: ', scores)
+        print('Evaluation scores: ', greedy_scores)
 
 def play_episode(env, agent, render=True, training=False, verbose=False):
     don = False
@@ -137,11 +160,14 @@ if __name__ == '__main__':
     EPSILON_BASE = .04
     POLYAK = .8
 
+    N_Q_COPIES = 3
+
     tb_writer = SummaryWriter(log_dir='runs/cartpole/{}'.format(START_TIME))
 
     agent = Agent(mem_size=MEM_SIZE, state_dim=STATE_DIM, action_dim=ACTION_DIM,
                   hidden_layers=HIDDEN_LAYERS, training_interval=TRAINING_INTERVAL,
-                  expl_time=EXPLORATION_TIME, eps_decay_time=EPSILON_DECAY_TIME, eps_base=EPSILON_BASE, polyak=POLYAK)
+                  expl_time=EXPLORATION_TIME, eps_decay_time=EPSILON_DECAY_TIME, eps_base=EPSILON_BASE, polyak=POLYAK,
+                  n_q_copies=N_Q_COPIES)
 
     train_agent(agent, n_episodes=10000, verbose=True, tb_writer=tb_writer)
 
